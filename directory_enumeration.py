8a2deb5504fcc5c3df7a6e80ee4d6338d58a9261
avagyantigran <32682657+avagyantigran@users.noreply.github.com>
2026-02-02 09:54:20 +0400

    Update directory_enumeration.py

--- directory_enumeration.py
+++ directory_enumeration.py
@@ -1,20 +1,12 @@
 import requests
 
-# Ask user for target URL
-target_url = input("Enter target URL (example: http://10.0.2.3): ").strip()
-
-# Fix missing http://
-if not target_url.startswith(("http://", "https://")):
-    target_url = "http://" + target_url
-
+target_url = "http://10.0.2.3"
 wordlist = ["admin", "login", "config", "backup", "php", "uploads", "phpMyAdmin"]
-
-print(f"\n[*] Scanning target: {target_url}\n")
 
 for word in wordlist:
     url = f"{target_url}/{word}"
     try:
-        response = requests.get(url, timeout=3, allow_redirects=False)
+        response = requests.get(url, timeout=3)
 
         if response.status_code in [200, 301, 302, 403]:
             print(f"[+] Found Directory: {url} ({response.status_code})")
